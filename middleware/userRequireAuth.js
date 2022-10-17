4791d0f069bf08563910069170715f6f2bf59b0a
Al Amin <almin.cont@gmail.com>
2022-10-17 16:35:47 +0600

    some change on env shedule

--- middleware/userRequireAuth.js
+++ middleware/userRequireAuth.js
@@ -5,15 +5,19 @@ require("dotenv").config();
 const userRequireAuth = async (req, res, next) => {
 
   const { authorization } = req.headers;
+  
   if (!authorization) {
     res.status(401).send({ message: "Unauthorized User" });
   }
+  
 
   if (authorization && authorization.startsWith("Bearer")) {
     try {
+      
       const token = authorization.split(" ")[1];
+      
       const { _id } = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-      req.user = await UserModel.findById({ _id });
+      req.user = await UserModel.findById({ _id }).select("_id");
       next();
     } catch (error) {
       res.status(401).send({ message: "Unauthorized User" });
--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -6,38 +6,40 @@ const {
   updateSchedule,
   deleteSchedule,
 } = require("../controllers/scheduleController");
+const userRequireAuth = require("../middleware/userRequireAuth");
 const validation = require("../middleware/validationMiddleware");
 const { scheduleValidation } = require("../validations/scheduleValidation");
-// const userRequireAuth = require("../middleware/userRequireAuth");
+
 
 // express router
 const router = express();
+router.use(userRequireAuth)
 
 router.post(
-  "/createSchedule",
+  "/",
   // userRequireAuth,
   validation(scheduleValidation),
   createSchedule
 );
 router.get(
-  "/getSchedules",
+  "/",
   // userRequireAuth,
   getSchedules
 );
 router.get(
-  "/getSchedule/:id",
+  "/:id",
   // userRequireAuth,
   validation(scheduleValidation),
   getSchedule
 );
 router.put(
-  "/updateSchedule/:id",
+  "/:id",
   // userRequireAuth,
   validation(scheduleValidation),
   updateSchedule
 );
 router.delete(
-  "/deleteSchedule/:id",
+  "/:id",
   // userRequireAuth,
   validation(scheduleValidation),
   deleteSchedule
